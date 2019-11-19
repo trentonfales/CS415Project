e626521126cd06ee05dbc2dd00296497d2528dad
Kelsey Donavin <kelseydonavin@gmail.com>
2019-11-19 11:25:29 -0800

    Names in

--- AirlineSearchEngine.py
+++ AirlineSearchEngine.py
@@ -4,6 +4,9 @@ Collaborators: Kelsey Donavin, Connor Donegan, Trenton Fales, Hermes Obiang
 Course: Big Data
 '''
 
+server_name = "PostgreSQL 12"
+db_name = "CS415"
+#table names: airlines, airports, routes
 
 import pyodbc
 
@@ -16,7 +19,7 @@ cursor = conn.cursor()
 
 cursor = conn.cursor()
 
-cursor.execute('SELECT * FROM db_name.Table')
+# cursor.execute('SELECT * FROM db_name.Table')
 
 for row in cursor:
     print(row)
