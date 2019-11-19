f3d76f416faa7a81a5ab9c9655f5edc20dd2191b
Kelsey Donavin <kelseydonavin@gmail.com>
2019-11-19 11:22:29 -0800

    First commit, need to connect still

--- AirlineSearchEngine.py
+++ AirlineSearchEngine.py
@@ -4,3 +4,19 @@ Collaborators: Kelsey Donavin, Connor Donegan, Trenton Fales, Hermes Obiang
 Course: Big Data
 '''
 
+
+import pyodbc
+
+conn = pyodbc.connect('Driver={SQL Server};'
+                      'Server=server_name;'
+                      'Database=db_name;'
+                      'Trusted_Connection=yes;')
+
+cursor = conn.cursor()
+
+cursor = conn.cursor()
+
+cursor.execute('SELECT * FROM db_name.Table')
+
+for row in cursor:
+    print(row)
